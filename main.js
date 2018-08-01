c9ef859b0f1cdbede268c96fe67e2420fa09b402
penguinoloopo <40578433+penguinoloopo@users.noreply.github.com>
2018-08-01 14:06:32 -0400

    Fix main.js

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
+// Image switcher code
+
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/snake_with_buns.jpg') {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/snake_with_buns.jpg') {
       myImage.setAttribute ('src','images/snake_with_beans.png');
-    } else {
-      myImage.setAttribute ('src','images/snake_with_buns.jpg');
+	} else {
+	  myImage.setAttribute ('src','images/snake_with_buns.jpg');
+	}
+}
+
+// Personalized welcome message code
 
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
 function setUserName() {
-  var myName = prompt("Who's tryna see snake with buns");
+  var myName = prompt("Who's tryna see snake with buns?");
   localStorage.setItem('name', myName);
-  myHeading.textContent = "You're a gamer, " + myName;
+  myHeading.innerHTML = "You're a gamer, " + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = "You're a gamer, " + storedName;
 }
 
 myButton.onclick = function() {
   setUserName();
-}
-    }
 }
